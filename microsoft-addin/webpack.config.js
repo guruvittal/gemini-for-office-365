9958b62c6e27ea87203cd91dc29d8713b7234427
Guru Rangavittal <guruvittal@google.com>
2026-09-06 02:13:54 +0000

    fix(build): enable content-hashed bundle filenames and nginx anti-caching to bypass Mac WebKit cache

--- microsoft-addin/webpack.config.js
+++ microsoft-addin/webpack.config.js
@@ -115,6 +115,7 @@ module.exports = async (env, options) => {
     },
     output: {
       clean: true,
+      filename: dev ? "[name].js" : "[name].[contenthash:8].js",
     },
     resolve: {
       extensions: [".html", ".js"],
